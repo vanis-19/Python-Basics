7c75d261499220d8ee2c841842a158ae30999137
unknown <sjyothi.11sj@gmail.com>
2020-10-16 11:42:03 +0530

    reverse

---
+++ reverse.py
@@ -0,0 +1,19 @@
+s=input("enter name")
+#s=s[::-1]
+#print(s)
+
+'''i=len(s)-1
+output=" "
+while i>0:
+    output=output+s[i]
+    i=i-1
+print(output)
+
+r=reversed(s)
+output="".join(r)
+print(output)'''
+
+i=len(s)-1
+while i>0:
+    print(s[i])
+    i=i-1
